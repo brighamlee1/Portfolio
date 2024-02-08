49bdee27f2ca2c8ec848a733af96007531f34870
Brigham Broadbent <bbroadbent@midtownhi.com>
2024-02-08 15:53:58 -0600

    fixed: urls

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,44 +4,36 @@ function Projects() {
     const projects = [
         {
             id: 1,
-            name: 'Crypto Viewer',
-            image: 'https://user-images.githubusercontent.com/91819733/171486084-54bcb5da-02f0-4bad-a4c5-75b86adab312.png',
-            github: 'https://github.com/brighamlee1/crypto-viewer',
-            live: 'https://crypto-viewer-321.herokuapp.com/',
+            name: 'Jersey Closet',
+            image: 'https://github.com/brighamlee1/portfolio/assets/91819733/ea7e3a95-d2b6-40c4-8ee1-ae50009b5937',
+            github: 'https://github.com/brighamlee1/jersey-shop-frontend',
+            live: 'https://jersey-closet.netlify.app/jerseys',
             description: 'React, JavaScript, CSS, API',
         },
         {
             id: 2,
-            name: 'Twitter Clone',
-            image: 'https://user-images.githubusercontent.com/91819733/170277299-de38efa6-acfb-4c8c-a5ed-8805a0e309c5.png',
-            github: 'https://github.com/brighamlee1/Twitter-Clone',
-            live: 'https://twitter-mock-321.herokuapp.com/',
-            description: "Express, NodeJS, MongoDB, EJS, CSS",
+            name: 'Crypto Viewer',
+            image: 'https://user-images.githubusercontent.com/91819733/171486084-54bcb5da-02f0-4bad-a4c5-75b86adab312.png',
+            github: 'https://github.com/brighamlee1/crypto-viewer',
+            live: 'https://crypto-viewer.onrender.com/',
+            description: 'React, JavaScript, CSS, API',
         },
         {
             id: 3,
+            name: 'Twitter Clone',
+            image: 'https://user-images.githubusercontent.com/91819733/170277299-de38efa6-acfb-4c8c-a5ed-8805a0e309c5.png',
+            github: 'https://github.com/brighamlee1/Twitter-Clone',
+            live: 'https://twitter-clone-d5t8.onrender.com/',
+            description: "Express, NodeJS, MongoDB, EJS, CSS",
+        },
+        {
+            id: 4,
             name: 'War Card Game',
             image: 'https://user-images.githubusercontent.com/91819733/163030480-1e273496-4589-462d-83dd-6244e471194d.png',
             github: 'https://github.com/brighamlee1/War-Project-1',
             live: 'https://brighamlee1.github.io/War-Project-1/',
             description: 'HTML, CSS, JavaScript',
-        },
-        {
-            id: 4,
-            name: 'Pokedex',
-            image: 'https://user-images.githubusercontent.com/91819733/170913676-989f42fc-3bce-481a-921a-e37aeeca334c.png',
-            github: 'https://github.com/brighamlee1/poke-dex',
-            live: 'https://pokedex-321.herokuapp.com/pokedex',
-            description: 'Express, NodeJS, EJS, CSS',
-        },
-        {
-            id: 5,
-            name: 'Tamagotchi',
-            image: 'https://user-images.githubusercontent.com/91819733/170913977-a9333622-1e85-427a-93b4-b5c31de67e17.png',
-            github: 'https://github.com/brighamlee1/tamagotchi',
-            live: 'https://dog-tamagotchi.netlify.app/',
-            description: 'HTML, CSS, JavaScript',
-        },
+        }
     ]
 
     return (
